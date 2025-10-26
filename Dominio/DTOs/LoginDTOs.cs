6a432122f9276be9613167c43c1d5c06f04bbc36
Edielson <edielsoncosta.c@gmail.com>
2025-10-26 02:36:57 -0300

    subindo projeto para o repositório

---
+++ Dominio/DTOs/LoginDTOs.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minimalapi.Dtos
+{
+    public class LoginDTOs
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
