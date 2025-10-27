4de71b6b01ba48898652c1aa2a68aaf4507782fa
Edielson <edielsoncosta.c@gmail.com>
2025-10-27 02:48:31 -0300

    Criando ef de administradores

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Infraestrutura.Db;
+using Minimalapi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Configurar o DbContext antes de Build()
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", static (Minimalapi.Dtos.LoginDTOs loginDTO) =>
+app.MapPost("/login", (LoginDTOs loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
