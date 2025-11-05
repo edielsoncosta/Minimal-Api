414f4c60b171545b1e89c15109827111fe23caf2
Edielson <edielsoncosta.c@gmail.com>
2025-11-05 02:00:32 -0300

    POST de veículos adicionado

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.DTOs;
 using minimal_api.Dominio.ModelViews;
 using minimal_api.Infraestrutura.Db;
+using Minimalapi.Dominio.Entidades;
 using Minimalapi.Dtos;
 using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Dominio.Servicos;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,13 +27,16 @@ builder.Services.AddDbContext<DbContexto>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
-
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTOs loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTOs loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (administradorServico.Login(loginDTO) !=  null)
+    if (administradorServico.Login(loginDTO) != null)
     {
         return Results.Ok("Login com sucesso");
     }
@@ -38,8 +45,26 @@ app.MapPost("/login", ([FromBody] LoginDTOs loginDTO, IAdministradorServico admi
         return Results.Unauthorized();
     }
 });
+#endregion
 
+#region Veiculos
+app.MapPost("/Veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/Veiculo/{veiculo.Id}", veiculo);
+});
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
